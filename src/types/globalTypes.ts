5759a7d8adf20355e00da899d4edf6b62e292c42
Kevin Sites <ksites@restaurant365.com>
2022-01-03 21:07:40 +0200

    Add - Fetch Request API, Retrieve Resume Hook, Loading Screen, App Context

---
+++ src/types/globalTypes.ts
@@ -0,0 +1,11 @@
+export interface ResumeObject {
+  personal: PersonalSectionObject;
+}
+
+export interface PersonalSectionObject {
+  name: string;
+  about: string;
+  resume: string;
+  title: string;
+  aboutMeImage: string;
+}
