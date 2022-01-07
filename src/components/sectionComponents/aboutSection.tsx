37b23e2ca27cfaeaa9e4eaab55e9be53b94805f4
Kevin Sites <ksites@restaurant365.com>
2022-01-07 17:38:26 +0200

    Update - Remove commented code for testing

--- src/components/sectionComponents/aboutSection.tsx
+++ src/components/sectionComponents/aboutSection.tsx
@@ -22,42 +22,42 @@ const AboutSection = () => {
     }
   }, [resume]);
 
-  // useEffect(() => {
-  //   if (index === words.length) {
-  //     setBlinkEnd(true);
-  //     return;
-  //   }
+  useEffect(() => {
+    if (index === words.length) {
+      setBlinkEnd(true);
+      return;
+    }
 
-  //   if (
-  //     subIndex === words[index].length + 1 &&
-  //     index !== words.length - 1 &&
-  //     !reverse
-  //   ) {
-  //     setReverse(true);
-  //     return;
-  //   }
+    if (
+      subIndex === words[index].length + 1 &&
+      index !== words.length - 1 &&
+      !reverse
+    ) {
+      setReverse(true);
+      return;
+    }
 
-  //   if (subIndex === 0 && reverse) {
-  //     setReverse(false);
-  //     setIndex((prev) => prev + 1);
-  //     return;
-  //   }
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      setIndex((prev) => prev + 1);
+      return;
+    }
 
-  //   const timeout = setTimeout(() => {
-  //     setSubIndex((prev) => prev + (reverse ? -1 : 1));
-  //   }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 : 150, parseInt((Math.random() * 350).toString())));
+    const timeout = setTimeout(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, Math.max(reverse ? 75 : subIndex === words[index].length ? 1000 : 150, parseInt((Math.random() * 350).toString())));
 
-  //   return () => clearTimeout(timeout);
-  // }, [subIndex, index, reverse, words.length]);
+    return () => clearTimeout(timeout);
+  }, [subIndex, index, reverse, words.length]);
 
-  // useEffect(() => {
-  //   const timeout2 = setTimeout(() => {
-  //     if (!blinkEnd) {
-  //       setBlink((prev) => !prev);
-  //     }
-  //   }, 500);
-  //   return () => clearTimeout(timeout2);
-  // }, [blink, blinkEnd]);
+  useEffect(() => {
+    const timeout2 = setTimeout(() => {
+      if (!blinkEnd) {
+        setBlink((prev) => !prev);
+      }
+    }, 500);
+    return () => clearTimeout(timeout2);
+  }, [blink, blinkEnd]);
 
   return (
     <article className={`about-section-container`}>
